0d21013e1293bb627d2d064d40fed8e0a98ba917
212393860 <Yutao.Zhou1@ge.com>
2017-10-10 12:01:46 -0400

    Added tests in config and utils

---
+++ utils/backoff_test.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+  "testing"
+  "time"
+)
+
+func TestNewSimpleBackoff(t *testing.T) {
+  simpleBackoff := NewSimpleBackoff(time.Second, time.Minute, 0.2, 2)
+  if simpleBackoff == nil {
+    t.Error("Failed to create a new SimpleBackoff!")
+    t.Fail()
+  }
+}
+
+func TestDuration(t *testing.T) {
+  simpleBackoff := NewSimpleBackoff(time.Second, time.Minute, 0.2, 2)
+  if simpleBackoff == nil {
+    t.Error("Failed to create a new SimpleBackoff!")
+    t.Fail()
+  }
+  duration := simpleBackoff.Duration()
+  if duration < 0 {
+    t.Error("Failed to in testing Duration() function!")
+    t.Fail()
+  }
+}
+
+func TestAddJitter(t *testing.T) {
+  newDuration := AddJitter(time.Second, 0)
+  if newDuration.Nanoseconds() != 1000000000 {
+    t.Error("Failed to in testing AddJitter() function when Jitter is 0!")
+    t.Fail()
+  }
+  newDuration = AddJitter(time.Second, 99)
+  if newDuration.Nanoseconds() < 1000000000 {
+    t.Error("Failed to in testing AddJitter() function when Jitter is not zero!")
+    t.Fail()
+  }
+}
