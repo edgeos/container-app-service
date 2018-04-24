5a1ccf1d79dfa66b02370c39a43a56a0c6391c78
212393860 <Yutao.Zhou1@ge.com>
2018-04-24 13:22:03 -0400

    Fix build problem when golint site is down

--- scripts/fetch-deps.sh
+++ scripts/fetch-deps.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-go get \
-        github.com/golang/lint/golint
+RES=$( { go get github.com/golang/lint/golint; } 2>&1 )
+
+if [[ "${RES}" != *"unrecognized"* ]] && [["${RES}" != *"fatal"* ]]; then
+  echo "Success to get to github.com/golang/lint."
+else
+  echo "Failed to get to github.com/golang/lint, trying golint git repo now..."
+  mkdir -p $GOPATH/src/golang.org/x \
+    && git clone https://github.com/golang/lint.git $GOPATH/src/golang.org/x/lint \
+    && go get -u golang.org/x/lint/golint
+fi
